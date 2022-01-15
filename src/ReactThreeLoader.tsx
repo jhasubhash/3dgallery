fd458c5414f28c4289d8e54446bfd922d3ff6e2d
Subhash Jha <sujha@adobe.com>
2022-01-15 14:55:01 +0530

    fixing loading

--- src/ReactThreeLoader.tsx
+++ src/ReactThreeLoader.tsx
@@ -1,8 +1,10 @@
 import React, { Suspense } from "react";
 import styled from "styled-components";
 import { Canvas } from "@react-three/fiber";
-import { Environment, OrbitControls, useGLTF } from "@react-three/drei";
-import Model from "./Test1";
+import { Environment, Loader, OrbitControls, useGLTF } from "@react-three/drei";
+import Model from "./Test";
+//import Loader from "./Loader";
+//import testHdr from './assets/models/test.hdr'
 
 const MainWrapper = styled.div`
   text-align: center;
@@ -15,20 +17,27 @@ const MainWrapper = styled.div`
 
 function ReactThreeLoader() {
   return (
-    <Canvas shadows camera={{ position: [-10, 15, 15], fov: 50 }}>
-      <ambientLight intensity={0.1} />
-      <pointLight castShadow position={[10, 10, 10]} />
-      <Suspense fallback={null}>
-        <Environment files={"test.hdr"} preset={"night"} />
-        <Model />
-      </Suspense>
-      <OrbitControls
-        addEventListener={undefined}
-        hasEventListener={undefined}
-        removeEventListener={undefined}
-        dispatchEvent={undefined}
+    <>
+      <Canvas shadows camera={{ position: [-10, 15, 15], fov: 50 }}>
+        <ambientLight intensity={0.1} />
+        <pointLight castShadow position={[10, 10, 10]} />
+        <Suspense fallback={null}>
+          <Environment files={"./assets/model/test.hdr"} preset={"night"} />
+          <Model />
+        </Suspense>
+        <OrbitControls
+          addEventListener={undefined}
+          hasEventListener={undefined}
+          removeEventListener={undefined}
+          dispatchEvent={undefined}
+        />
+      </Canvas>
+      <Loader
+        innerStyles={{
+          margin: "50%",
+        }}
       />
-    </Canvas>
+    </>
   );
 }
 
